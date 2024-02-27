eb103579db84634c4ba4bb5c7eea90104e41a523
ymHan <onyxsard@gmail.com>
2024-02-27 11:18:01 +0900

    [ymhan]

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,16 +5,30 @@ import React, { useState, useEffect } from "react";
 import ImageList from '@mui/material/ImageList';
 import ImageListItem from '@mui/material/ImageListItem';
 import ImageListItemBar from '@mui/material/ImageListItemBar';
+import { useSearchParams } from "next/navigation";
 
 
 const Gallery: React.FC = () => {
   const [images, setImages] = useState<string[]>([]);
+  const searchParams = useSearchParams();
+  let path = searchParams.get('path') as string;
+
+  const getDates = () => {
+    const date = new Date().toISOString();
+    const formattedDate = date.split('T')[0];
+
+    return formattedDate.replace(/-/g, '');
+  }
+
+  !(path) ? path = getDates() : null;
 
   useEffect(() => {
     const fetchImages = async () => {
       try {
-        const res = await fetch("https://api.4dist.com/v1/mwc");
+        const res = await fetch(`https://api.4dist.com/v1/mwc/${path}`);
+
         const { data } = await res.json();
+        if (!data) return;
 
         const images = data.filter((d: any) => d.thumbnail.includes(".jpg")).map((d: any) => d.thumbnail);
         setImages(data);
@@ -33,21 +47,14 @@ const Gallery: React.FC = () => {
     const result = await fetch(`https://api.4dist.com/v1/mwc/html?filename=${fileName}`);
   };
 
-  const getDates = () => {
-    const date = new Date().toISOString();
-    const formattedDate = date.split('T')[0];
-
-    return formattedDate.replace(/-/g, '');
-  }
-console.log(getDates())
   return (
     <div>
       <ImageList>
         {images.map((item) => (
-          <ImageListItem key={item.index}>
+          <ImageListItem key={item.video}>
             {/* eslint-disable-next-line @next/next/no-img-element */}
             <img
-              src={`http://cdn.4dist.com/de-01/mwc/${getDates()}/${item.thumbnail}?w=248&fit=crop&auto=format`}
+              src={`http://cdn.4dist.com/de-01/mwc/${path}/${item.thumbnail}?w=248&fit=crop&auto=format`}
               alt={item.index}
               loading="lazy"
               onClick={() => {
